9357cc6a0a846b6eafb527b4dcec5af97d5bd381
Victor Tapia <victortapiaegana@gmai.com>
2026-04-02 18:16:36 -0300

    ADD: Ajustes finales

--- djangowallet/settings.py
+++ djangowallet/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,12 +76,34 @@ WSGI_APPLICATION = 'djangowallet.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'DjangoWallet',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+DATABASE_URL = "postgresql://djangowallet_user:SKGWZ1Kf1ihR6BnoWEMTX7NRXMIBkujq@dpg-d77dnpma2pns73cvdi9g-a.ohio-postgres.render.com/djangowallet"
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=600,
+        ssl_require=True # Render requiere SSL para conexiones externas
+    )
 }
+
 
 
 # Password validation
