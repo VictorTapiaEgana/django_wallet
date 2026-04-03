3702d3e19dd5cc4ac95e5cf754823d58fc37ee39
Victor Tapia <victortapiaegana@gmai.com>
2026-04-02 21:15:38 -0300

    ADD: Registro modelos en admin"

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+admin.site.site_header = "DjangoWallet Admin"
+admin.site.site_title = "DjangoWallet Admin Portal"
+admin.site.index_title = "Welcome to DjangoWallet Admin Portal"
+
+
+@admin.register(Transferencia)
+class TransferenciaAdmin(admin.ModelAdmin):
+    list_display = ('remitente', 'destinatario', 'monto', 'fecha') # Columnas que verás
+    list_filter = ('fecha',) # Filtro lateral por fecha
+    search_fields = ('remitente__usuario__username', 'destinatario__usuario__username')
